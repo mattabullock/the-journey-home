5d8504f8b847e48702e45b699a2c5ad23c797a1a
Woofles <salty.cea.dog@gmail.com>
2014-02-21 10:54:31 -0600

    networkmultiplayer functionality works

---
+++ game/Assets/scripts/NetworkManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	public GameObject levelCam;
+	SpawnSpot[] spawnSpots;
+
+
+	// Use this for initialization
+	void Start () {
+		spawnSpots = GameObject.FindObjectsOfType<SpawnSpot> ();
+		Connect ();
+	}
+
+	void Connect() {
+		//PhotonNetwork.offlineMode = true;
+		PhotonNetwork.ConnectUsingSettings ("v001");
+	}
+
+	void OnGUI() {
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString ());
+	}
+
+	void OnJoinedLobby() {
+		PhotonNetwork.JoinRandomRoom ();
+	}
+
+	void OnPhotonRandomJoinFailed() {
+		PhotonNetwork.CreateRoom (null);
+	}
+
+	void OnJoinedRoom() {
+		spawnPlayer ();
+	}
+
+	void spawnPlayer() {
+
+		SpawnSpot mySpawn = spawnSpots [Random.Range (0, spawnSpots.Length)];
+
+		levelCam.SetActive (false);
+
+		GameObject myPlayerGO = (GameObject) PhotonNetwork.Instantiate ("PlayerController", mySpawn.transform.position, mySpawn.transform.rotation, 0);
+		((MonoBehaviour) myPlayerGO.GetComponent ("FPSInputController")).enabled = true;
+		((MonoBehaviour) myPlayerGO.GetComponent ("MouseLook")).enabled = true;
+		((MonoBehaviour) myPlayerGO.GetComponent ("CharacterMotor")).enabled = true;
+		myPlayerGO.transform.FindChild("Main Camera").gameObject.SetActive(true);
+
+	}
+
+}
