1a735b6071c353b384ffc8b6a6920e5498323749
Alex <fastback23@gmail.com>
2014-02-23 12:55:30 -0600

    FPS changes

--- game/Assets/scripts/FP_Shooter.cs
+++ game/Assets/scripts/FP_Shooter.cs
@@ -5,7 +5,7 @@ public class FP_Shooter : MonoBehaviour {
 
 	public GameObject bullet_prefab;
 	public Camera theCam;
-//	public GameObject theGun;
+	public GameObject theGun;
 	float bulletPulse = 20f;
 
 	// Use this for initialization
@@ -16,8 +16,8 @@ public class FP_Shooter : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown("Fire1")){
-//			theGun.animation["Default Take"].speed = 10;
-//			theGun.animation.Play();
+			theGun.animation["Default Take"].speed = 10;
+			theGun.animation.Play();
 			GameObject the_bullet = (GameObject) PhotonNetwork.Instantiate ("bullet", theCam.transform.position + theCam.transform.forward, theCam.transform.rotation, 0);
 			the_bullet.rigidbody.AddForce(theCam.transform.forward * bulletPulse, ForceMode.Impulse);
 		}
