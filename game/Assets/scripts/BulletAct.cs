ed420731e0da086fd8bcfc16ce1aa25fdfb6b4ae
Alex <fastback23@gmail.com>
2014-02-23 11:51:59 -0600

    FPS elements
    Added Bullet and Target Prefabs
    Added FPS scripts

---
+++ game/Assets/scripts/BulletAct.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletAct : MonoBehaviour {
+
+	float lifespan = 3.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifespan -= Time.deltaTime;
+		if (lifespan <= 0) {
+			Explode();
+		}
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		print (collision.gameObject.tag);
+
+		if (collision.gameObject.tag == "Enemy") {
+			Destroy(gameObject);
+			Destroy(collision.gameObject);
+		}
+	}
+
+	void Explode () {
+		Destroy (gameObject);
+	}
+}
---
+++ game/Assets/scripts/FP_Shooter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FP_Shooter : MonoBehaviour {
+
+	public GameObject bullet_prefab;
+	public Camera theCam;
+	public GameObject theGun;
+	float bulletPulse = 20f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")){
+			theGun.animation["Default Take"].speed = 10;
+			theGun.animation.Play();
+			GameObject the_bullet = (GameObject) Instantiate (bullet_prefab, theCam.transform.position + theCam.transform.forward, theCam.transform.rotation);
+			the_bullet.rigidbody.AddForce(theCam.transform.forward * bulletPulse, ForceMode.Impulse);
+		}
+	}
+}
