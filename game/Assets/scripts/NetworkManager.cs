8e82a6e8d12f0feb83822192ff7ab4a5107ceb35
Woofles <salty.cea.dog@gmail.com>
2014-02-25 16:12:03 -0600

    hoorah

--- game/Assets/scripts/NetworkManager.cs
+++ game/Assets/scripts/NetworkManager.cs
@@ -1,127 +1,54 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
+using System.Collections;
 
 public class NetworkManager : MonoBehaviour {
 	
-	public GameObject standbyCamera;
+	public GameObject levelCam;
 	SpawnSpot[] spawnSpots;
 	
-	public bool offlineMode = false;
-	
-	bool connecting = false;
-	
-	List<string> chatMessages;
-	int maxChatMessages = 5;
 	
 	// Use this for initialization
 	void Start () {
-		spawnSpots = GameObject.FindObjectsOfType<SpawnSpot>();
-		PhotonNetwork.player.name = PlayerPrefs.GetString("Username", "Awesome Dude");
-		chatMessages = new List<string>();
-	}
-	
-	void OnDestroy() {
-		PlayerPrefs.SetString("Username", PhotonNetwork.player.name);
-	}
-	
-	public void AddChatMessage(string m) {
-		GetComponent<PhotonView>().RPC ("AddChatMessage_RPC", PhotonTargets.AllBuffered, m);
-	}
-	
-	[RPC]
-	void AddChatMessage_RPC(string m) {
-		while(chatMessages.Count >= maxChatMessages) {
-			chatMessages.RemoveAt(0);
-		}
-		chatMessages.Add(m);
+		spawnSpots = GameObject.FindObjectsOfType<SpawnSpot> ();
+		Screen.lockCursor = true;
+		Connect ();
 	}
 	
 	void Connect() {
-		PhotonNetwork.ConnectUsingSettings( "MultiFPS v001" );
+		//PhotonNetwork.offlineMode = true;
+		PhotonNetwork.ConnectUsingSettings ("v001");
 	}
 	
 	void OnGUI() {
-		GUILayout.Label( PhotonNetwork.connectionStateDetailed.ToString() );
-		
-		if(PhotonNetwork.connected == false && connecting == false ) {
-			GUILayout.BeginArea( new Rect(0, 0, Screen.width, Screen.height) );
-			GUILayout.BeginHorizontal();
-			GUILayout.FlexibleSpace();
-			GUILayout.BeginVertical();
-			GUILayout.FlexibleSpace();
-			
-			GUILayout.BeginHorizontal();
-			GUILayout.Label("Username: ");
-			PhotonNetwork.player.name = GUILayout.TextField(PhotonNetwork.player.name);
-			GUILayout.EndHorizontal();
-			
-			
-			if( GUILayout.Button("Single Player") ) {
-				connecting = true;
-				PhotonNetwork.offlineMode = true;
-				OnJoinedLobby();
-			}
-			
-			if( GUILayout.Button("Multi Player") ) {
-				connecting = true;
-				Connect ();
-			}
-			GUILayout.FlexibleSpace();
-			GUILayout.EndVertical();
-			GUILayout.FlexibleSpace();
-			GUILayout.EndHorizontal();
-			GUILayout.EndArea();
-		}
-		
-		if(PhotonNetwork.connected == true && connecting == false) {
-			GUILayout.BeginArea( new Rect(0, 0, Screen.width, Screen.height) );
-			GUILayout.BeginVertical();
-			GUILayout.FlexibleSpace();
-			
-			foreach(string msg in chatMessages) {
-				GUILayout.Label(msg);
-			}
-			
-			GUILayout.EndVertical();
-			GUILayout.EndArea();
-			
-		}
-		
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString ());
 	}
 	
 	void OnJoinedLobby() {
-		Debug.Log ("OnJoinedLobby");
-		PhotonNetwork.JoinRandomRoom();
+		PhotonNetwork.JoinRandomRoom ();
 	}
 	
 	void OnPhotonRandomJoinFailed() {
-		Debug.Log ("OnPhotonRandomJoinFailed");
-		PhotonNetwork.CreateRoom( null );
+		PhotonNetwork.CreateRoom (null);
 	}
 	
 	void OnJoinedRoom() {
-		Debug.Log ("OnJoinedRoom");
-		
-		connecting = false;
-		SpawnMyPlayer();
+		spawnPlayer ();
 	}
 	
-	void SpawnMyPlayer() {
-		AddChatMessage("Spawning player: " + PhotonNetwork.player.name);
+	void spawnPlayer() {
 		
-		if(spawnSpots == null) {
-			Debug.LogError ("WTF?!?!?");
-			return;
-		}
+		SpawnSpot mySpawn = spawnSpots [Random.Range (0, spawnSpots.Length)];
 		
-		SpawnSpot mySpawnSpot = spawnSpots[ Random.Range (0, spawnSpots.Length) ];
-		GameObject myPlayerGO = (GameObject)PhotonNetwork.Instantiate("PlayerController", mySpawnSpot.transform.position, mySpawnSpot.transform.rotation, 0);
-		standbyCamera.SetActive(false);
+		levelCam.SetActive (false);
 		
-		//((MonoBehaviour)myPlayerGO.GetComponent("FPSInputController")).enabled = true;
-		((MonoBehaviour)myPlayerGO.GetComponent("MouseLook")).enabled = true;
-		((MonoBehaviour)myPlayerGO.GetComponent("PlayerMovement")).enabled = true;
-		((MonoBehaviour)myPlayerGO.GetComponent("PlayerShooting")).enabled = true;
+		GameObject myPlayerGO = (GameObject) PhotonNetwork.Instantiate ("PlayerController", mySpawn.transform.position, mySpawn.transform.rotation, 0);
+		((MonoBehaviour) myPlayerGO.GetComponent ("FPSInputController")).enabled = true;
+		((MonoBehaviour) myPlayerGO.GetComponent ("MouseLook")).enabled = true;
+		((MonoBehaviour) myPlayerGO.GetComponent ("CharacterMotor")).enabled = true;
+		//		((MonoBehaviour) myPlayerGO.GetComponent ("PlayerMovement")).enabled = true
+		((MonoBehaviour) myPlayerGO.GetComponent ("PlayerShooting")).enabled = true;
 		myPlayerGO.transform.FindChild("Main Camera").gameObject.SetActive(true);
+		
 	}
+	
 }
