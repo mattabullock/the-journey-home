d8cfb9032c06f59b53d1a043febdf296837c8d22
Woofles <salty.cea.dog@gmail.com>
2014-02-28 23:08:04 -0600

    added server browser and other stuff

---
+++ game/Assets/scripts/MainMenu.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public GUISkin styles;
+	Vector2 viewPoint = Vector2.one;
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.automaticallySyncScene = true;
+		Connect ();
+	}
+	
+	void Update() {
+		//something about updating respawns should go here?!
+	}
+	
+	void Connect() {
+		PhotonNetwork.ConnectUsingSettings ("v005");
+	}
+
+	void OnGUI() {
+
+		float ScreenX = Screen.width * 0.25f;
+		float ScreenY = Screen.height * 0.2f;
+		float areaWidth = Screen.width * 0.5f;
+		float areaHeight = Screen.height * 0.5f;
+
+		GUILayout.BeginArea (new Rect (ScreenX, ScreenY, areaWidth, areaHeight), styles.window);
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("Server");
+		GUILayout.Label("Players");
+		GUILayout.EndHorizontal();
+		viewPoint = GUILayout.BeginScrollView(viewPoint, false, true);
+		foreach (RoomInfo room in PhotonNetwork.GetRoomList())
+		{
+
+			GUILayout.BeginHorizontal();
+			GUILayout.Label(room.name);
+			GUILayout.Label(room.playerCount + "/" + room.maxPlayers);
+			if(GUILayout.Button("Connect")) {
+				PhotonNetwork.JoinRoom(room.name);
+			}
+			GUILayout.EndHorizontal();
+		}
+		GUILayout.EndScrollView ();
+		GUILayout.BeginHorizontal ();
+		GUILayout.TextField ("");
+		if (GUILayout.Button("Create Room")) {
+			PhotonNetwork.CreateRoom("name",true,true,4);
+		}
+		GUILayout.EndHorizontal ();
+
+		GUILayout.EndArea ();
+		
+	}
+
+	void loadScene() {
+		PhotonNetwork.LoadLevel ("testscene");
+	}
+
+	void OnJoinedRoom() {
+		loadScene ();
+	}
+}
--- game/Assets/scripts/NetworkManager.cs
+++ game/Assets/scripts/NetworkManager.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 using System.Collections;
 
-public class NetworkManager : MonoBehaviour {
+public class NetworkManager : Photon.MonoBehaviour {
 	
-	public GameObject levelCam;
+//	public GameObject levelCam;
 	public static SpawnSpot[] spawnSpots;
 	
 	
@@ -11,42 +11,19 @@ public class NetworkManager : MonoBehaviour {
 	void Start () {
 		spawnSpots = GameObject.FindObjectsOfType<SpawnSpot> ();
 		Screen.lockCursor = true;
-		Connect ();
-	}
-
-	void Update() {
-		//something about updating respawns should go here?!
-	}
-	
-	void Connect() {
-		PhotonNetwork.ConnectUsingSettings ("v005");
+		if (PhotonNetwork.isMasterClient) {
+			PhotonNetwork.InstantiateSceneObject ("LightSystem", new Vector3(6.5f, .668f, 15.83f), Quaternion.identity, 0, null);
+		}
+		spawnPlayer ();
 	}
 	
 	void OnGUI() {
 		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString ());
 	}
 	
-	void OnJoinedLobby() {
-		PhotonNetwork.JoinRandomRoom ();
-	}
-	
-	void OnPhotonRandomJoinFailed() {
-		PhotonNetwork.CreateRoom (null);
-	}
-
-	void OnCreatedRoom() {
-		PhotonNetwork.InstantiateSceneObject ("LightSystem", new Vector3(6.5f, .668f, 15.83f), Quaternion.identity, 0, null);
-	}
-	
-	void OnJoinedRoom() {
-		spawnPlayer ();
-	}
-	
 	public void spawnPlayer() {
 		
 		SpawnSpot mySpawn = spawnSpots [Random.Range (0, spawnSpots.Length)];
-		
-		levelCam.SetActive (false);
 		
 		GameObject myPlayerGO = (GameObject) PhotonNetwork.Instantiate ("PlayerController", mySpawn.transform.position, mySpawn.transform.rotation, 0);
 		((MonoBehaviour) myPlayerGO.GetComponent ("MouseLook")).enabled = true;
