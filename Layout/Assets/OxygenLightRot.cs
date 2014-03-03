460d7891f6e25a2670cc792314e12ab88125cac0
Ashley Montgomery <aemontgomery756@gmail.com>
2014-03-02 19:03:35 -0600

    Layout scene with enemy pathfinding

---
+++ Layout/Assets/OxygenLightRot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OxygenLightRot : MonoBehaviour {
+
+	float rotateSpeed = 180f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Update() {
+
+		transform.Rotate (rotateSpeed * Time.deltaTime, 0, 0) ;
+	}
+}
