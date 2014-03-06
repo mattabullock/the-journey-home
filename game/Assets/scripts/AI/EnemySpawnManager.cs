70d946f4f4c9ebb75c067932e8a602c2be3ad23f
Woofles <salty.cea.dog@gmail.com>
2014-03-06 15:23:11 -0600

    enemyspawnmanager

---
+++ game/Assets/scripts/AI/EnemySpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawnManager : MonoBehaviour {
+
+	public float timer = 0f;
+	public float spawnDelay = 3f;
+
+	// Use this for initialization
+	void Start () {
+		if (PhotonNetwork.isMasterClient) {
+			for(int i = 0; i < 10; i++) {
+				PhotonNetwork.Instantiate("Test Enemy", new Vector3(1.899121f, 0.5744562f, -3.08994f), Quaternion.identity, 0, null);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PhotonNetwork.isMasterClient) {
+			timer += Time.deltaTime;
+			if (timer > spawnDelay) {
+				timer = 0;
+				PhotonNetwork.Instantiate ("Test Enemy", new Vector3 (1.899121f, 0.5744562f, -3.08994f), Quaternion.identity, 0, null);
+			}
+		}
+	}
+}
---
+++ game/Assets/scripts/Systems/EngineSystem.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EngineSystem : SystemBase {
+
+	// Use this for initialization
+	protected override void Start () {
+		base.Start ();
+		currentHitPoints = 0;
+	}
+	
+	
+	
+	// Update is called once per frame
+	protected override void Update () {
+		base.Update ();
+		if(currentHitPoints >= 100 && !SpawnManager.win) {
+			SpawnManager.win = true;
+		}
+	}
+	
+	protected override void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+		base.OnPhotonSerializeView (stream, info);
+		if (stream.isWriting) {
+
+		} else if (stream.isReading) {
+
+		}
+	}
+	
+	[RPC]
+	protected override void repair (float amt) {
+		if (currentHitPoints >= hitPoints) {
+			return;	
+		} else if (currentHitPoints + amt > hitPoints) {
+			currentHitPoints = hitPoints;
+		} else {
+			currentHitPoints += amt;
+		}
+		
+		currHealthBarLength = healthBarLength * (currentHitPoints / hitPoints);
+	}
+	
+	[RPC]
+	protected override void TakeDamage(float amt) {
+		
+		if (currentHitPoints <= 0) {
+			return;
+		} else if (currentHitPoints - amt <= 0) {
+			currentHitPoints = 0;
+		} else {
+			currentHitPoints -= amt;
+			trigger ();
+		}	
+		
+		currHealthBarLength = healthBarLength * (currentHitPoints / hitPoints);
+	}
+	
+	[RPC]
+	protected void trigger() {
+		Debug.Log ("System oh no!!");
+	}
+	
+	public EngineSystem getEngineSystem() {
+		return this;
+	}
+	
+	public bool isDown() {
+		return down;
+	}
+	
+}
+
