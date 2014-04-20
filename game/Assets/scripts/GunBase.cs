fc76fc3f32d9a48ebeee57c807165c7ef5dd9ed0
Woofles <salty.cea.dog@gmail.com>
2014-04-20 01:41:39 -0500

    new gun, new model for health bay

---
+++ game/Assets/scripts/GunBase.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunBase : MonoBehaviour {
+
+	public float maxAmmo = 20;
+	public float reloadCooldown = 3f;
+	public float fireRate = 0.1f;
+	public float damage = 25f;
+	public float ammo;
+
+	void Start () {
+		ammo = maxAmmo;
+	} 
+	
+}
