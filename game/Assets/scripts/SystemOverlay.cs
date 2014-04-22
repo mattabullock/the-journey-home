910bd718a2ae829afbbcfcb1e21f461d6f3de7b8
Woofles <salty.cea.dog@gmail.com>
2014-04-22 17:21:24 -0500

    menu

---
+++ game/Assets/scripts/SystemOverlay.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SystemOverlay : MonoBehaviour
+{
+//	GUISkin newSkin;
+
+	public void thePauseMenu() {
+		//layout start
+		GUI.BeginGroup(new Rect(0, 0, Screen.width, Screen.height));
+		//the menu background box
+		GUI.Box(new Rect(0,0, Screen.width, Screen.height), "");
+		///////pause menu buttons
+		//game resume button
+		if(GUI.Button(new Rect(Screen.width/2-(Screen.width/8), Screen.height/2-(2*Screen.height/16), Screen.width/4, Screen.height/16), "Resume")) {
+			//resume the game
+			Time.timeScale = 1.0f;
+			Destroy(this);
+			Screen.showCursor = false;
+		}
+
+		//main menu return button (level 0)
+		if(GUI.Button(new Rect(Screen.width/2-(Screen.width/8), Screen.height/2-(Screen.height/16), Screen.width/4, Screen.height/16), "Main Menu")) {
+			Time.timeScale = 1.0f;
+			PhotonNetwork.LeaveRoom();
+			Application.LoadLevel("MainMenu");	
+		}
+		//quit button
+		if(GUI.Button(new Rect(Screen.width/2-(Screen.width/8), Screen.height/2, Screen.width/4, Screen.height/16), "Quit")) {
+			Application.Quit();
+		}
+		//layout end
+		GUI.EndGroup();
+	}
+	void OnGUI () {
+		//load GUI skin
+//		GUI.skin = newSkin;
+		//show the mouse cursor
+		Screen.showCursor = true;
+		//run the pause menu script
+		thePauseMenu();
+		Debug.Log ("hoorah!");
+	}
+	
+}
---
+++ game/Assets/scripts/UpdateScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateScript: MonoBehaviour {
+
+	bool menuScreen = false;
+	SystemOverlay sysO;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Escape") && !menuScreen) {
+			Debug.Log ("escape!");
+			//pause the game
+			menuScreen = true;
+			Time.timeScale = 0;
+			//show the pause menu
+			sysO = gameObject.AddComponent<SystemOverlay> (); 
+			sysO.enabled = true;
+		} else if (Input.GetButtonDown ("Escape") && menuScreen) {
+			Time.timeScale = 1.0f;
+			Destroy(sysO);
+			Screen.showCursor = false;
+			menuScreen = false;
+		}
+	}
+}
